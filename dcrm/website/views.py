09e90315edb4525cb282c1674ad99fee58292095
Aditya Ayare <adityasayare2003@gmail.com>
2023-08-26 03:06:02 +0530

    Tables added

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
+
 
 def home(request):
+    records=Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -15,7 +18,8 @@ def home(request):
         else:
             messages.success(request, "There was an error logging in, Please try again later 😓")
             return redirect('home')
-    return render(request, 'home.html', {})
+    else:
+        return render(request, 'home.html', {'records':records})
 
 def logout_user(request):
     logout(request)
@@ -35,5 +39,13 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form': form})
+    
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record=Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be Logged In to view this page 😒")
+        return redirect('home')
